85b2948b5133e9c50c3deda507ab80935b505f9f
Andrei Vaduva <andreivaduva@Valentins-MacBook-Pro.local>
2019-08-13 15:49:07 +0300

    Updated entrypoint for github action

--- .github/actions/build-action/entrypoint.sh
+++ .github/actions/build-action/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
-echo "-------------------------"
-env
-echo "-------------------------"
-ls -lah
-
 $KUBE_CERTS_DOWNLOADER
 
 docker build -t hivecluster .
 docker tag hivecluster lynxaegon/hivecluster
 docker push lynxaegon/hivecluster
 
-kubectl --kubeconfig="/tmp/certs.yaml" set image deployment exoskeleton-frontend exoskeleton-frontend=$(docker inspect --format='{{index .RepoDigests 0}}' lynxaegon/hivecluster:latest)
+kubectl --kubeconfig="certs.yaml" set image deployment exoskeleton-frontend exoskeleton-frontend=$(docker inspect --format='{{index .RepoDigests 0}}' lynxaegon/hivecluster:latest)
