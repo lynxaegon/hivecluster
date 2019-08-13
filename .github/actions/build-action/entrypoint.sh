5e8408771d46e39749eb9f258aa608493fe71d7f
Andrei Vaduva <andreivaduva@Valentins-MacBook-Pro.local>
2019-08-13 16:39:58 +0300

    Updated entrypoint

--- .github/actions/build-action/entrypoint.sh
+++ .github/actions/build-action/entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
+# Pull Kube Certificates from DO
 curl --request GET -s --url https://api.digitalocean.com/v2/kubernetes/clusters/${DO_KUBE_CLUSTER}/kubeconfig --header "authorization: Bearer ${DO_API_KEY}" > ./kube_certs.yaml
 
-cd src && npm install && cd ..
+# NPM install
+cd src 
+npm install
+cd ..
 
+# Docker login
 docker login
 
+# Docker build
 docker build -t hivecluster .
 docker tag hivecluster lynxaegon/hivecluster
 docker push lynxaegon/hivecluster
 
+# Kube image update
 kubectl --kubeconfig="kube_certs.yaml" set image deployment exoskeleton-frontend exoskeleton-frontend=$(docker inspect --format='{{index .RepoDigests 0}}' lynxaegon/hivecluster:latest)
-
